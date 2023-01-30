727a614eced37826a131dbbea6b234b90ba60283
Michael Scholl <mls2scholl@gmail.com>
2023-01-29 23:41:21 -0600

    Breaking changes and working on the getting the DB

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    db: 'mongodb://localhost:27017/reactdb'
+    db: 'mongodb://192.168.1.155:27017/reactdb'
     };
     
--- src/components/Particles.js
+++ src/components/Particles.js
@@ -1,116 +1,95 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import { polygonPathName, loadPolygonPath } from "tsparticles-path-polygon";
+//import hexagonOptions from './particle-options/hexagon.json';
+import testOptions from './particle-options/test.json';
+import { hexagonOptions } from './particles-options';
+
+
+import(`./particles-options/${selectedFile}`)
+.then(options => {
+  <Particles id="tsparticles" options={options} init={particlesInit} loaded={particlesLoaded} />
+});
+
+
+// Import Custom CSS
+import "../App.css";
 
 const ParticlesPage = () => {
-    const particlesInit = useCallback(async engine => {
-        console.log(engine);
-        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
-        //await loadFull(engine);
-        //https://codesandbox.io/s/42ps4y?file=/src/App.js:324-2223
-        //https://codepen.io/matteobruni/pen/ZExaQyB
-        ///https://particles.js.org/
-        await loadPolygonPath(engine);
-    }, []);
 
-    const particlesLoaded = useCallback(async container => {
-        await console.log(container);
-    }, []);
+  const [showOverlay, setShowOverlay] = useState(false);
+  const [selectedFile, setSelectedFile] = useState('');
+  const [optionFileLocation] = '';
+  const options = require(`./particles-options/${selectedFile}`);
 
-    return (
-        <Particles
-          options={{
-            fpsLimit: 60,
-            particles: {
-              color: {
-                value: "#fbdb04",
-                animation: {
-                  enable: true,
-                  speed: 10
-                }
-              },
-              move: {
-                attract: {
-                  enable: true,
-                  rotate: {
-                    distance: 100,
-                    x: 2000,
-                    y: 2000
-                  }
-                },
-                direction: "none",
-                enable: true,
-                outModes: {
-                  default: "destroy"
-                },
-                path: {
-                  clamp: false,
-                  enable: true,
-                  delay: {
-                    value: 0
-                  },
-                  generator: polygonPathName,
-                  options: {
-                    sides: 6,
-                    turnSteps: 30,
-                    angle: 30
-                  }
-                },
-                random: false,
-                speed: 3,
-                straight: true,
-                trail: {
-                  fillColor: "#000",
-                  length: 20,
-                  enable: true
-                }
-              },
-              number: {
-                density: {
-                  enable: true,
-                  area: 800
-                },
-                value: 0
-              },
-              opacity: {
-                value: 0.5
-              },
-              shape: {
-                type: "circle"
-              },
-              size: {
-                value: 2
-              }
-            },
-            background: {
-              color: "#3c5570"
-            },
-            fullScreen: {
-              zIndex: -1
-            },
-            detectRetina: true,
-            emitters: {
-              direction: "none",
-              rate: {
-                quantity: 1,
-                delay: 0.25
-              },
-              size: {
-                width: 0,
-                height: 0
-              },
-              position: {
-                x: 50,
-                y: 50
-              }
-            }
-          }}
-          init={particlesInit}
-        />
-      );
+
+  const fileList = [
+    './particle-options/test.json',
+    './particle-options/hexagon.json',
+    './particle-options/starry.json'
+  ];
+
+  const handleFileSelect = (e) => {
+    setSelectedFile(e.target.value);
+  }
+
+  const particlesInit = useCallback(async engine => {
+    console.log(engine);
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+    //https://codesandbox.io/s/42ps4y?file=/src/App.js:324-2223
+    //https://codepen.io/matteobruni/pen/ZExaQyB
+    ///https://particles.js.org/
+    //await loadPolygonPath(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async container => {
+    await console.log(container);
+  }, []);
+
+  return (
+    <div className="App">
+      <button 
+        className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded-full"
+        onClick={() => setShowOverlay(!showOverlay)}
+      >
+        Open Overlay
+      </button>
+      {showOverlay && (
+        <div className="overlay fixed top-0 left-0 w-1/5 h-screen h-1/4 h-screen bg-gray-900 p-8">
+          <div className="bg-white rounded-lg p-4">
+            <div className="flex items-center justify-between">
+              <h2 className="text-lg font-medium text-indigo-500">Select a File</h2>
+              <button
+                className="text-indigo-500 font-medium focus:outline-none"
+                onClick={() => setShowOverlay(false)}
+              >
+                <svg className="w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M6 18L18 6M6 6l12 12"></path></svg>
+              </button>
+            </div>
+            <div className="my-4">
+              <select className="block w-full border rounded p-2" onChange={handleFileSelect}>
+                {fileList.map((file, index) => (
+                  <option key={index} value={file}>{file}</option>
+                ))}
+              </select>
+            </div>
+            <button 
+              className="bg-indigo-500 hover:bg-indigo-700 text-white font-medium py-2 px-4 rounded-full w-full"
+              onClick={() => setShowOverlay(false)}
+            >
+              Close
+            </button>
+            {selectedFile && <p className="text-indigo-500">Selected file: {selectedFile}</p>}
+          </div>
+        </div>
+      )}
+      <Particles id="tsparticles" options={require(`./particles-options/${selectedFile}`)} init={particlesInit} loaded={particlesLoaded} />
+    </div>
+  );
 };
 
 export default ParticlesPage;
